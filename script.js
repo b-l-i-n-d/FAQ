4311a84d524a677320dc3208d8ad12f43a2261ec
Fahim Faisal <42368238+b-l-i-n-d@users.noreply.github.com>
2021-10-02 19:20:32 +0600

    Opens one at a time

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ let li = document.querySelectorAll('.faq-text li')
 
 for (var i = 0; i < li.length; i++) {
     li[i].addEventListener('click', (e) => {
+        
+        let open = document.querySelector('.faq-text li.show-answer');
+        if (open !== null) {
+            open.classList.remove('show-answer');
+        };
+
         let clickedLi;
         let s = e.target.classList.contains('show-answer');
         console.log(s);
+
         if(e.target.classList.contains('question-arrow')) {
             clickedLi = e.target.parentElement;
         } else {
